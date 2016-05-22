935d6cf32380075cc4d83ef5a55cdd7d33be9cc0
Sergiy Zaschipas <serge@uacoders.com>
2016-05-22 11:15:21 +0300

    workaround with IRLib

--- boiler.ino
+++ boiler.ino
@@ -1,29 +1,14 @@
 
-
-
-
-//Sample using LiquidCrystal library
-
-#include <LiquidCrystal.h>
+#include <EEPROM.h>
+#include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-/*******************************************************
-
-  This program will test the LCD panel and the buttons
-  Mark Bramwell, July 2010
-
-********************************************************/
-
-// select the pins used on the LCD panel
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+#include <LiquidCrystal_I2C.h>
+#include <IRLib.h>
 
 
-// Pass our oneWire reference to Dallas Temperature.
+#define MY_PROTOCOL NEC
 
-
-// define some values used by the panel and buttons
-int lcd_key     = 0;
-int adc_key_in  = 0;
 #define btnRIGHT  0
 #define btnUP     1
 #define btnDOWN   2
@@ -31,50 +16,28 @@ int adc_key_in  = 0;
 #define btnSELECT 4
 #define btnNONE   5
 
-#define ONE_WIRE_BUS 2
+#define IR_BUS 2
+#define ONE_WIRE_BUS 4
 
-#define RELAY1  4                        
-#define RELAY2  11                       
-#define RELAY3  12                        
 
 #define INFO_MODE 0;
 #define SET_TEMPERATURE_MODE 1
 #define SET_RELAY_MODE 2
 
-int mode = INFO;
+int relay_map[3] = {10, 11, 12}; // relay pins
+boolean relay_state[3];
+
+int mode = INFO_MODE;
+int lcd_key     = 0;
+int adc_key_in  = 0;
 
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+IRrecv My_Receiver(IR_BUS);
+IRdecode My_Decoder;
 
-int count = 0;
-// read the buttons
-
-int read_LCD_buttons()
-{
-  adc_key_in = analogRead(0);      // read the value from the sensor
-  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
-  // we add approx 50 to those values and check to see if we are close
-  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
-  // For V1.1 us this threshold
-  if (adc_key_in < 50)   return btnRIGHT;
-  if (adc_key_in < 250)  return btnUP;
-  if (adc_key_in < 450)  return btnDOWN;
-  if (adc_key_in < 650)  return btnLEFT;
-  if (adc_key_in < 850)  return btnSELECT;
-
-  // For V1.0 comment the other threshold and use the one below:
-  /*
-    if (adc_key_in < 50)   return btnRIGHT;
-    if (adc_key_in < 195)  return btnUP;
-    if (adc_key_in < 380)  return btnDOWN;
-    if (adc_key_in < 555)  return btnLEFT;
-    if (adc_key_in < 790)  return btnSELECT;
-  */
-
-
-  return btnNONE;  // when all others fail, return this...
-}
 
 struct Settings {
   float delta;
@@ -83,130 +46,167 @@ struct Settings {
 
 Settings settings;
 
+
 void setup()
 {
+   lcd.init();                      // initialize the lcd
 
-  
+  for (byte i = 0; i < 3; i++) {
+    pinMode(relay_map[i], OUTPUT);
+  }
+  power_relay_off();
+
+  My_Receiver.No_Output();
+
+  My_Receiver.enableIRIn();
+
   EEPROM.get(0, settings);
-  pinMode(RELAY1, OUTPUT);       
-  pinMode(RELAY2, OUTPUT);
-  pinMode(RELAY3, OUTPUT);
 
-  digitalWrite(RELAY1,HIGH);          // Turns Relay Off
-  digitalWrite(RELAY2,HIGH);
-  digitalWrite(RELAY3,HIGH);
-  
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
+  Serial.println("Boiler Debug Console:");
 
   sensors.begin();
-  lcd.begin(16, 2);              // start the library
+
+  lcd.backlight();
   lcd.setCursor(0, 1);
-  lcd.print("                "); 
-  lcd.print("Init:           "); 
-  
+  lcd.print("                ");
+  lcd.print("Init:           ");
+
   // init settings
-  
+
 }
 
-byte sensor_map[6][3]={
- {0,1,2},
- {0,2,1},
- {1,0,2},
- {1,2,0},
- {2,0,1},
- {2,1,0}
+void loop() {
+
+
+  if (My_Receiver.GetResults(&My_Decoder)) {
+    My_Decoder.decode();
+    Serial.println(My_Decoder.decode_type, HEX);
+    Serial.println(My_Decoder.value, HEX);
+    if (My_Decoder.decode_type == MY_PROTOCOL) {
+      process_key(My_Decoder.value);
+
+    }
+    My_Receiver.resume();
+  } else {
+
+    check_idle();
+    sensors.requestTemperatures();
+    if (mode == 0) {
+      print_temperature();
+      logic();
+      print_relay_status();
+    }
+    lcd.setCursor(0, 0);
+  }
+
+}
+
+
+byte sensors_map[6][3] = {
+  {0, 1, 2},
+  {0, 2, 1},
+  {1, 0, 2},
+  {1, 2, 0},
+  {2, 0, 1},
+  {2, 1, 0}
 };
 
-void print_temperature(){
+void print_temperature() {
   lcd.setCursor(0, 0);           // move cursor to second line "1" and 9 spaces over
-  for(int i=0;i<3;i++)
-    lcd.print(sensors.getTempCByIndex(sensors_map[settings.sensor_map_id][i]),1);      // display seconds elapsed since power-up
+  for (int i = 0; i < 3; i++) {
+    lcd.print(sensors.getTempCByIndex(sensors_map[settings.sensor_map_id][i]), 1);     // display seconds elapsed since power-up
     lcd.print(";");
   }
-    lcd.print("  ");  
+  lcd.print("   ");
 }
 
 
-unsigned long last_keypress=0;
+unsigned long last_keypress = 0;
 
-void check_idle(){
+void check_idle() {
 
-  if (abs(millis()-last_keypress)>5000){
-    mode = 0;  
+  if (abs(millis() - last_keypress) > 5000) {
+    mode = 0;
   }
-  
+
 }
 
-void logic(){
+void logic() {
   float t_out = sensors.getTempCByIndex(sensors_map[settings.sensor_map_id][2]); //outcome water t
   float t_in = sensors.getTempCByIndex(sensors_map[settings.sensor_map_id][1]);  //income water t
   float t_outdoor = sensors.getTempCByIndex(sensors_map[settings.sensor_map_id][0]); // outdoor t
   float target_t;
   float hysteresis = 1;
-  target_t =43-0.8*t_outdoor;
-  if ( target_t+hysteresis < t_in ){
-    power_relay_on()
-  } else if(target_t-hysteresis>t_in){
+  target_t = 43 - 0.8 * t_outdoor;
+  if ( target_t + hysteresis < t_in ) {
+    power_relay_on();
+  } else if (target_t - hysteresis > t_in) {
     power_relay_off();
   }
 }
 
-void relay_status();
 
-void power_relay_on(){
-  digitalWrite(RELAY1,LOW);
-  digitalWrite(RELAY2,LOW);
-  digitalWrite(RELAY3,LOW);
+
+void relay_off(byte relay_id) {
+  relay_state[relay_id] = false;
+  digitalWrite(relay_map[relay_id], HIGH);
+}
+
+void relay_on(byte relay_id) {
+  relay_state[relay_id] = true;
+  digitalWrite(relay_map[relay_id], LOW);
+}
+
+
+void power_relay_on() {
+  for (byte i = 0; i < 3; i++) {
+    relay_on(i);
+  }
 };
 
-void power_relay_off(){
-  digitalWrite(RELAY1,HIGH);
-  digitalWrite(RELAY2,HIGH);
-  digitalWrite(RELAY3,HIGH);
+void power_relay_off() {
+  for (byte i = 0; i < 3; i++) {
+    relay_off(i);
+  }
 };
 
 
-void process_key(int lcd_key){
-  
+void process_key(int lcd_key) {
+
   last_keypress = millis();
-  
+
   switch (lcd_key)               // depending on which button was pushed, we perform an action
   {
     case btnRIGHT:
-        break;
+      break;
     case btnLEFT:
-        break;
+      break;
     case btnUP:
-        break;
+      break;
     case btnDOWN:
-        break;
+      break;
     case btnSELECT:
-        break;
+      break;
     case btnNONE:
-        break;
+      break;
   }
 
 }
 
-void loop()
-{
- 
- lcd_key = read_LCD_buttons();
- if(lcd_key!=5){
-    process_key(lcd_key);
- }
- 
- check_idle();
- sensors.requestTemperatures();
- if(mode==0){
-  print_temperature();
-  logic();
-  print_relay_status();
- }
- lcd.setCursor(0, 0); 
-  return ;
-
+void print_relay_status() {
+  lcd.setCursor(0, 1);
+  lcd.print("HEAD:");
+  for (byte i = 0; i < 3; i++) {
+    
+    if (relay_state[i]) {
+      lcd.print("[*]");
+    } else {
+      lcd.print("[ ]");
+    }
+    
+  }
+  lcd.print("    ");
 }
 
 
